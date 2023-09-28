353f93ed32197fd3a32ca1ea567b313c718b51fa
IDDQD-code <oleg20026@gmail.com>
2023-09-28 10:41:51 +0300

    Created first template

--- ReportGenerator/app/views.py
+++ ReportGenerator/app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.template.response import TemplateResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    return TemplateResponse(request, "index.html", context={'header' : 'Testing django', 'message' : 'hello user!'})
+
+def user(request):
+    age = request.GET.get('age')
+    name = request.GET.get('name')
+    return HttpResponseBadRequest("Доступ заблокирован: недостаточно лет")
